d6b6d742d4e3938e0049e5301f361ba8d488d86e
IzcoatlLlanos <heizllanosgo@ittepic.edu.mx>
2023-04-28 01:38:08 -0600

    Ya funciona la vista del director de hoteles y moteles, crear, moficar y cambiar estatus, falta eliminar

---
+++ src/app/interface/icon-color.ts
@@ -0,0 +1,5 @@
+export interface IconColor {
+    iconName: string;
+    iconColor: string;
+    msj: string;
+}
--- src/app/service/usuario.service.ts
+++ src/app/service/usuario.service.ts
@@ -8,54 +8,68 @@ import { Usuario } from '../interface/usuario';
 export class UsuarioService {
 
   private usuarios: Usuario[] = [];
-  private gerentes: Usuario[] = [];
 
   constructor() {
     this.usuarios.push({
-      IdUsuarioOK: "1",
-      IdUsuarioBK: "Jaimito",
+      IdUsuarioOK: "sanjose_5678",
+      IdUsuarioBK: "sanjose_5678",
       Nombres: "Jaime",
       Apellidos: "Robles",
       Hotel: "San Jose",
       Rol: "1",
       Clave: "123",
       CorreoEle: "jaimito@ittepic.edu.mx",
-      Celular: "3112345678"
+      Celular: "3112345678",
+      Generated: false,
+      Activo: true
     },{
-      IdUsuarioOK: "2",
-      IdUsuarioBK: "Llanitos",
+      IdUsuarioOK: "lahiguera_5678",
+      IdUsuarioBK: "lahiguera_5678",
       Nombres: "Hector",
       Apellidos: "Llanos",
       Clave: "456",
       Hotel: "La Higuera",
       Rol: "2",
       CorreoEle: "Hector@ittepic.edu.mx",
-      Celular: "3112345678"
+      Celular: "3112345678",
+      Generated: false,
+      Activo: true
     },{
-      IdUsuarioOK: "3",
-      IdUsuarioBK: "Jaredcito",
+      IdUsuarioOK: "losarroyos_5678",
+      IdUsuarioBK: "losarroyos_5678",
       Nombres: "Jared",
       Apellidos: "Zavala",
       Clave: "789",
       Hotel: "Los Arroyos",
       Rol: "2",
       CorreoEle: "Jared@ittepic.edu.mx",
-      Celular: "3112345678"
+      Celular: "3112345678",
+      Generated: false,
+      Activo: false
     });
 
-    this.gerentes = this.usuarios.filter(usuario => usuario.Rol == '2');
+    
   }
   
   public getUsuarios(): Usuario[] {
     return this.usuarios;
   }
 
-  public newUsuario(usr: Usuario) {
+  public addUsuario(usr: Usuario) {
     this.usuarios.push(usr);
+  }
+  
+  public updateUsuario(usr: Usuario, i: number) {
+    this.usuarios[i] = usr;
+  }
+
+  public updateUsrActivo(i: number, valor: boolean) {
+    
+    this.usuarios[i].Activo = valor;
   }
 
   public getGerentes(): Usuario[] {
-    return this.gerentes;
+    return this.usuarios.filter(usuario => usuario.Rol == '2');
   }
 
   public login(usr: string, pwd: string): Usuario|undefined {
